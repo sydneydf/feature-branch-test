a88f4a1eeafe660989058a02153d2106cbac90d3
sydneydf <maximumsupernova@gmail.com>
2020-08-17 16:33:27 +0800

    v0.01-Initial-Game-Board-Branch-Commit

---
+++ tic-tac-toe.py
@@ -0,0 +1,10 @@
+def game_board():
+    game_board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+    print(f"{game_board[6]}+{game_board[7]}+{game_board[8]}+")
+    print(f"-+-+-")
+    print(f"{game_board[3]}+{game_board[4]}+{game_board[5]}+")
+    print(f"-+-+-")
+    print(f"{game_board[0]}+{game_board[1]}+{game_board[2]}+")
+
+    game_board()
